b40b44083fabac228f0088573b82c8a17f763242
beawillis <beawillis2005@gmail.com>
2026-03-11 17:18:12 -0700

    initial commit: My details project

--- intro.js
+++ intro.js
@@ -1,1 +1,12 @@
-console.log("Hello, World");
+//intro.js
+
+//Define my name, age and favourite programming language
+
+const user = {
+    name: "Elisha Alvin Bifandhuba",
+    age: 21,
+    favourite_programming_language: "JavaScript",
+
+};  
+
+console.log("My name is " +user.name +", I am " +user.age +" years old and my favourite_programming_language is " +user.favourite_programming_language +".");
